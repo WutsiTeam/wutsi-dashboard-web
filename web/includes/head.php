7dd82bc7086087322f4bed6306f1ce8dc83c794c
htchepannou <htchepannou@expedia.com>
2020-09-23 05:56:29 -0400

    update index

---
+++ web/includes/head.php
@@ -0,0 +1,31 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="images/favicon.ico" type="image/ico" />
+
+<title>Wutsi Admin Dashboard</title>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-daterangepicker/3.0.5/daterangepicker.min.css" integrity="sha512-rBi1cGvEdd3NmSAQhPWId5Nd6QxE8To4ADjM2a6n0BrqQdisZ/RPUlm0YycDzvNL1HHAh1nKZqI0kSbif+5upQ==" crossorigin="anonymous" />
+
+<!-- NProgress -->
+<!--
+<link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
+-->
+<!-- iCheck -->
+<!--
+<link href="../vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+-->
+
+<!-- bootstrap-progressbar -->
+<!--
+<link href="../vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+-->
+<!-- JQVMap -->
+<!--
+<link href="../vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet" />
+-->
+
--- web/index.php
+++ web/index.php
@@ -1,7 +1,316 @@
-<html>
-<body>
+<!DOCTYPE html>
+<html lang="en">
 
-HELLO WORLD
+<head>
+    <?php include 'includes/head.php';?>
+</head>
 
+<body class="nav-md">
+    <div class="container body">
+        <div class="main_container">
+
+            <!-- sidebar -->
+            <div class="col-md-3 left_col">
+                <div class="left_col scroll-view">
+                    <?php include 'includes/sidebar.php'; ?>
+                </div>
+            </div>
+            <!-- /sidebar -->
+
+            <!-- top navigation -->
+            <?php include 'includes/topnav.php'; ?>
+            <!-- /top navigation -->
+
+            <!-- page content -->
+            <div class="right_col" role="main">
+
+
+                <!-- top tiles -->
+                <div class="row" style="display: inline-block;">
+                    <div class="tile_count">
+                        <div class="col-md-4 col-sm-4 mx-3 tile_stats_count">
+                            <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
+                            <div class="count">2500</div>
+                            <span class="count_bottom"><i class="green">4% </i> All time</span>
+                        </div>
+                        <div class="col-md-4 col-sm-4 mx-3 tile_stats_count">
+                            <span class="count_top"><i class="fa fa-clock-o"></i>Total Read Time</span>
+                            <div class="count">123.50</div>
+                            <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> All Time</span>
+                        </div>
+                        <div class="col-md-4 col-sm-4 mx-3 tile_stats_count">
+                            <span class="count_top"><i class="fa fa-user"></i> Total Views</span>
+                            <div class="count green">2,500</div>
+                            <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> All Time</span>
+                        </div>
+                        <div class="col-md-4 col-sm-4 mx-3 tile_stats_count">
+                            <span class="count_top"><i class="fa fa-user"></i> Read Sugested</span>
+                            <div class="count">4,567</div>
+                            <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>12% </i> All Time</span>
+                        </div>
+
+                    </div>
+                </div>
+                <!-- /top tiles -->
+
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 ">
+                        <div class="dashboard_graph">
+
+                            <div class="row x_title">
+                                <div class="col-md-6">
+                                    <h3>Network Activities <small>Graph title sub-title</small></h3>
+                                </div>
+                                <div class="col-md-6">
+                                    <div id="reportrange" class="pull-right" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
+                                        <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
+                                        <span>September 30, 20120 - Novmber 28, 200</span> <b class="caret"></b>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-md-9 col-sm-9 ">
+                                <div id="chart_plot_03" class="demo-placeholder"></div>
+                            </div>
+                            <div class="col-md-3 col-sm-3  bg-white">
+                                <div class="x_title">
+                                    <h2>Top Campaign Performance</h2>
+                                    <div class="clearfix"></div>
+                                </div>
+
+                                <div class="col-md-12 col-sm-12 ">
+                                    <div>
+                                        <p>Facebook Campaign</p>
+                                        <div class="">
+                                            <div class="progress progress_sm" style="width: 76%;">
+                                                <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="80"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <p>Twitter Campaign</p>
+                                        <div class="">
+                                            <div class="progress progress_sm" style="width: 76%;">
+                                                <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="60"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-12 col-sm-12 ">
+                                    <div>
+                                        <p>Conventional Media</p>
+                                        <div class="">
+                                            <div class="progress progress_sm" style="width: 76%;">
+                                                <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="40"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                            </div>
+
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+
+                </div>
+                <br />
+
+                <div class="row">
+                    <div class="col-md-4 col-sm-4 ">
+                        <div class="x_panel tile fixed_height_320">
+                            <div class="x_title">
+                                <h2>App Versions</h2>
+                                <ul class="nav navbar-right panel_toolbox">
+                                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                                    </li>
+                                    <li class="dropdown">
+                                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                            <a class="dropdown-item" href="#">Settings 1</a>
+                                            <a class="dropdown-item" href="#">Settings 2</a>
+                                        </div>
+                                    </li>
+                                    <li><a class="close-link"><i class="fa fa-close"></i></a>
+                                    </li>
+                                </ul>
+                                <div class="clearfix"></div>
+                            </div>
+                            <div class="x_content">
+                                <h4>App Usage across versions</h4>
+                                <div class="widget_summary">
+                                    <div class="w_left w_25">
+                                        <span>0.1.5.2</span>
+                                    </div>
+                                    <div class="w_center w_55">
+                                        <div class="progress">
+                                            <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 66%;">
+                                                <span class="sr-only">60% Complete</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="w_right w_20">
+                                        <span>123k</span>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+
+                                <div class="widget_summary">
+                                    <div class="w_left w_25">
+                                        <span>0.1.5.3</span>
+                                    </div>
+                                    <div class="w_center w_55">
+                                        <div class="progress">
+                                            <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 45%;">
+                                                <span class="sr-only">60% Complete</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="w_right w_20">
+                                        <span>53k</span>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <div class="widget_summary">
+                                    <div class="w_left w_25">
+                                        <span>0.1.5.4</span>
+                                    </div>
+                                    <div class="w_center w_55">
+                                        <div class="progress">
+                                            <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">
+                                                <span class="sr-only">60% Complete</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="w_right w_20">
+                                        <span>23k</span>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <div class="widget_summary">
+                                    <div class="w_left w_25">
+                                        <span>0.1.5.5</span>
+                                    </div>
+                                    <div class="w_center w_55">
+                                        <div class="progress">
+                                            <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 5%;">
+                                                <span class="sr-only">60% Complete</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="w_right w_20">
+                                        <span>3k</span>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <div class="widget_summary">
+                                    <div class="w_left w_25">
+                                        <span>0.1.5.6</span>
+                                    </div>
+
+                                    <div class="w_center w_55">
+                                        <div class="progress">
+                                            <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 2%;">
+                                                <span class="sr-only">60% Complete</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="w_right w_20">
+                                        <span>1k</span>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+
+
+
+
+                </div>
+
+
+                <div class="row">
+
+
+
+                    <div class="col-md-8 col-sm-8 ">
+
+
+
+                        <div class="row">
+
+                            <div class="col-md-12 col-sm-12 ">
+                                <div class="x_panel">
+                                    <div class="x_title">
+                                        <h2>Visitors location <small>geo-presentation</small></h2>
+                                        <ul class="nav navbar-right panel_toolbox">
+                                            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                                            </li>
+                                            <li class="dropdown">
+                                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                                    <a class="dropdown-item" href="#">Settings 1</a>
+                                                    <a class="dropdown-item" href="#">Settings 2</a>
+                                                </div>
+                                            </li>
+                                            <li><a class="close-link"><i class="fa fa-close"></i></a>
+                                            </li>
+                                        </ul>
+                                        <div class="clearfix"></div>
+                                    </div>
+                                    <div class="x_content">
+                                        <div class="dashboard-widget-content">
+                                            <div class="col-md-4 hidden-small">
+                                                <h2 class="line_30">125.7k Views from 60 countries</h2>
+
+                                                <table class="countries_list">
+                                                    <tbody>
+                                                        <tr>
+                                                            <td>United States</td>
+                                                            <td class="fs15 fw700 text-right">33%</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>France</td>
+                                                            <td class="fs15 fw700 text-right">27%</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Germany</td>
+                                                            <td class="fs15 fw700 text-right">16%</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Spain</td>
+                                                            <td class="fs15 fw700 text-right">11%</td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td>Cameroon</td>
+                                                            <td class="fs15 fw700 text-right">10%</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                            <div id="world-map-gdp" class="col-md-8 col-sm-12 " style="height:230px;"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+            <!-- /page content -->
+        </div>
+    </div>
+
+<?php include 'includes/footer.php';?>
 </body>
+
 </html>
