a4d6a6eff6705481ad346cab9fc5aeeb4f6bc15b
htchepannou <htchepannou@expedia.com>
2020-09-23 06:22:55 -0400

    update

--- web/index.php
+++ web/index.php
@@ -24,29 +24,27 @@
             <!-- page content -->
             <div class="right_col" role="main">
 
-
                 <!-- top tiles -->
                 <div class="row" style="display: inline-block;">
                     <div class="tile_count">
                         <div class="col-md-4 col-sm-4 mx-3 tile_stats_count">
                             <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
                             <div class="count">2500</div>
-                            <span class="count_bottom"><i class="green">4% </i> All time</span>
                         </div>
+
                         <div class="col-md-4 col-sm-4 mx-3 tile_stats_count">
                             <span class="count_top"><i class="fa fa-clock-o"></i>Total Read Time</span>
                             <div class="count">123.50</div>
-                            <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> All Time</span>
                         </div>
+
                         <div class="col-md-4 col-sm-4 mx-3 tile_stats_count">
-                            <span class="count_top"><i class="fa fa-user"></i> Total Views</span>
+                            <span class="count_top"><i class="far fa-eye"></i> Total Views</span>
                             <div class="count green">2,500</div>
-                            <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> All Time</span>
                         </div>
+
                         <div class="col-md-4 col-sm-4 mx-3 tile_stats_count">
-                            <span class="count_top"><i class="fa fa-user"></i> Read Sugested</span>
+                            <span class="count_top"><i class="fab fa-readme"></i> Read Sugested</span>
                             <div class="count">4,567</div>
-                            <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>12% </i> All Time</span>
                         </div>
 
                     </div>
