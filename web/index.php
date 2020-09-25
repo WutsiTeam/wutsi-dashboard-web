7a43fe042cddba86ec9ec2c2e71a0a838d726027
htchepannou <htchepannou@expedia.com>
2020-09-25 18:57:20 -0400

    ...

--- web/index.php
+++ web/index.php
@@ -85,7 +85,8 @@
             $.get(url)
                 .done(function(response){
                     console.log('GET', url, response);
-                    me.innerHTML = response.value;
+                    console.log('>>>', me.innerHTML);
+                    me.innerHTML = n_formatter(response.value, 1);
                 })
                 .fail(function(error){
                     console.error('GET', url, error);
@@ -93,5 +94,25 @@
             ;
         });
     });
+
+    function n_formatter(num, digits) {
+        var si = [
+            { value: 1, symbol: "" },
+            { value: 1E3, symbol: "k" },
+            { value: 1E6, symbol: "M" },
+            { value: 1E9, symbol: "G" },
+            { value: 1E12, symbol: "T" },
+            { value: 1E15, symbol: "P" },
+            { value: 1E18, symbol: "E" }
+        ];
+        var rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+        var i;
+        for (i = si.length - 1; i > 0; i--) {
+            if (num >= si[i].value) {
+                break;
+            }
+        }
+        return (num / si[i].value).toFixed(digits).replace(rx, "$1") + si[i].symbol;
+    }
 </script>
 </html>
