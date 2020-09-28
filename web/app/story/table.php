46438fd21d0644fe4041dadeee0f8cfdfbdad97d
htchepannou <htchepannou@expedia.com>
2020-09-28 17:10:30 -0400

    return mock response

--- web/app/story/table.php
+++ web/app/story/table.php
@@ -1,16 +1,14 @@
 <?php
     include '../functions/http.php';
 
-    var $now = date();
-    $request = array (
-        'publishedStartDate' => date('DATE_W3C', strtotime($now. ' - 7 days')),
-        'sortBy' => 'published',
-        'sortOrder' => 'descending',
-        'status' => 'published',
-        'live' => true,
-        'limit' => 50,
-        'offset' => 0
-    );
+//    $request = array (
+//        'sortBy' => 'published',
+//        'sortOrder' => 'descending',
+//        'status' => 'published',
+//        'live' => true,
+//        'limit' => 50
+//    );
+//    $response = http_post(http_blog_url('/v1/story/search'), $request);
     $response = array (
         array(1, 'This is a sample story', 'Ray Sponsible', '2020-01-30', 44, '2m 30s'),
         array(2, 'This is a another story', 'John Smith', '2020-03-07', 1435, '1m 19s')
